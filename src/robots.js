e746c797b3679f70ef7c5a191d9f9122f7d08bea
kisborg <mark.borgstrom@gmail.com>
2020-06-29 18:02:16 +0200

    added source files

---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Sophie',
+        username: 'Kukacmaki',
+        email: 'kukac@maki.com'
+    },
+    {
+        id: 2,
+        name: 'Marcika',
+        username: 'Kisborg',
+        email: 'kisborg@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Dancus',
+        username: 'Borg',
+        email: 'borg@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Chocho',
+        username: 'Fosomedve',
+        email: 'chochovagyok@jatsszvelem.com'
+    },
+    {
+        id: 5,
+        name: 'Brandon',
+        username: 'Kistacsko',
+        email: 'kistacsko@chochojatek.com'
+    },
+    {
+        id: 6,
+        name: 'Oswald',
+        username: 'Papagaj',
+        email: 'papagaj@chochojatek.com'
+    },
+    {
+        id: 7,
+        name: 'Jake',
+        username: 'jakepluss',
+        email: 'jakepluss@chochojatek.com'
+    },
+    {
+        id: 8,
+        name: 'Bjorn',
+        username: 'nagymacko',
+        email: 'nagymacko@chochojatek.com'
+    }
+    
+]
